d7decd9c9e9ce01a12badfa2457664b0d7be6701
Paul Codding <pcodding@gmail.com>
2014-04-07 13:45:30 -0500

    Added script to grab relevant Ambari logs for support.

---
+++ Installation/gather_ambari_logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+FILENAME="ambari_logs_`hostname`"
+ARTIFACT_BASEPATH="/tmp"
+
+function gather_server_logs {
+        zip -r $ARTIFACT_BASEPATH/$FILENAME.zip /var/log/ambari-server /var/run/ambari-server/bootstrap > /dev/null
+}
+function gather_agent_logs {
+        zip -r $ARTIFACT_BASEPATH/$FILENAME.zip /var/log/ambari-agent /var/lib/ambari-agent/data > /dev/null
+}
+
+function capture_metadata {
+        rpm -qa | grep ambari > $ARTIFACT_BASEPATH/$FILENAME.txt
+}
+
+function print_instructions {
+        echo -e "[-] SFTP the following files from `hostname`:\n\t$ARTIFACT_BASEPATH/$FILENAME.zip\n\t$ARTIFACT_BASEPATH/$FILENAME.txt"
+}
+
+echo "[*] Gathering Ambari Logs"
+if [ -d /var/log/ambari-server ]; then
+        gather_server_logs
+fi
+if [ -d /var/log/ambari-agent ]; then
+        gather_server_logs
+fi
+capture_metadata
+print_instructions
